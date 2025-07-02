79a7c29994b8b274bd28ff078911ba1d116aaae6
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2025-07-02 14:25:56 +0530

    fixed spelling mistake

--- server/models/clubs.cjs
+++ server/models/clubs.cjs
@@ -1,40 +1,40 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const clubSchema=new mongoose.Schema({
-    clubName:{
-        type:String,
-        required:true
+const clubSchema = new mongoose.Schema({
+    clubName: {
+        type: String,
+        required: true
     },
-     clubPresident:{
-        type:String,
-        required:true
+    clubPresident: {
+        type: String,
+        required: true
     },
-     description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-     departmment:{
-        type:String,
-        required:true
+    department: {
+        type: String,
+        required: true
     },
-     members:[{
-        type:String,
-        required:true
+    members: [{
+        type: String,
+        required: true
     }],
-    faculty:{
-        type:String,
-        required:true
+    faculty: {
+        type: String,
+        required: true
     },
-    createdAt:{
-        type:String,
-        required:true
+    createdAt: {
+        type: String,
+        required: true
     },
-    updatedAt:{
-        type:String,
-        required:true
+    updatedAt: {
+        type: String,
+        required: true
     }
 });
 
-const Club=new mongoose.model("Club",clubSchema);
+const Club = new mongoose.model("Club", clubSchema);
 
-module.exports=Club;
+module.exports = Club;
